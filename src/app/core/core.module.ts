586f1d57ba1d683bcfcabe36abec4d7cb733690a
Calum Moir-Mattox <calummm@gmail.com>
2022-08-05 11:30:56 +1000

    Initialised sveltekit

--- src/app/core/core.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { BrowserModule } from '@angular/platform-browser';
-
-@NgModule({
-  declarations: [],
-  imports: [BrowserModule],
-})
-export class CoreModule {}
