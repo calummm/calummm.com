2b7eb88e7c07d3827e44104b3488ef3d3bdd627f
Calum Moir-Mattox <calummm@gmail.com>
2022-08-08 20:27:40 +1000

    add mdsvex

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<header>
+    <a href="/">Home</a>
+  
+    <nav>
+      <ul>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+        <li>
+          <a href="/author">Author</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from "path"
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	resolve: {
+        alias: {
+            // '$lib': path.resolve('./src/lib/'),
+            // '$base': path.resolve('./src/baseApp'),
+        },
+    },
 	plugins: [sveltekit()]
 };
 
