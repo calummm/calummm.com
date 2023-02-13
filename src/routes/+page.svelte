5a4265ebc69f1dd4622b8a1c6022a70cf09e4944
Calum Moir-Mattox <calummm@gmail.com>
2023-02-13 21:25:27 +0700

    Add homepage routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<article class="md:container md:mx-auto p-2">
+  <h1>Welcome</h1>
+
+  <ul class="mt-3">
+    <li>
+      <a
+        class="flex gap-2 border-2 rounded border-black p-3"
+        href="/blog/2022-08-24_welcome"
+        ><div class="w-24 h-16 border border-black" />
+        Welcome blog post</a
+      >
+    </li>
+  </ul>
+</article>
