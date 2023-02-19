48a074a8669fff2f605f212e8dec7f4ad2cde63b
Calum Moir-Mattox <calummm@gmail.com>
2023-02-19 12:22:31 +0700

    Add basic route animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,24 @@
   import '../app.css';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
+
+  import { fade } from 'svelte/transition';
+
+  export let data;
 </script>
 
 <div class="flex flex-col min-h-screen">
   <Header />
 
-  <main class="grow">
-    <slot />
-  </main>
+  {#key data.currentRoute}
+    <main
+      class="grow"
+      in:fade={{ duration: 150, delay: 150 }}
+      out:fade={{ duration: 150 }}
+    >
+      <slot />
+    </main>
+  {/key}
 </div>
 
 <Footer />
