e14acaa113f9370c042fb29c0442bf8f55ced8ed
Calummm <calummm@gmail.com>
2025-08-23 22:30:50 +1000

    fix theme fouc

--- _includes/theme.js
+++ _includes/theme.js
@@ -1,21 +1,16 @@
-const storedLightTheme =
-	localStorage.getItem('lightTheme') === 'true' ||
-	window.matchMedia('(prefers-color-scheme: light)').matches;
-
 const root = document.documentElement;
-root.style.setProperty('--first-color', 'red');
-root.dataset.theme = storedLightTheme ? 'light' : 'dark';
-// console.dir(root);
 
-// setTimeout(() => {
-// 	const checkboxLightTheme = document.getElementById('light-theme');
-// 	checkboxLightTheme.checked = storedLightTheme;
+if (root.dataset.theme) {
+	const checkboxLightTheme = document.getElementById('light-theme');
+	checkboxLightTheme.checked = root.dataset.theme === 'light';
 
-// 	checkboxLightTheme.addEventListener('change', (e) => {
-// 		if (e.target.checked) {
-// 			localStorage.setItem('lightTheme', 'true');
-// 		} else {
-// 			localStorage.removeItem('lightTheme');
-// 		}
-// 	});
-// }, 0);
+	checkboxLightTheme.addEventListener('change', (e) => {
+		if (e.target.checked) {
+			localStorage.setItem('lightTheme', 'true');
+		} else {
+			localStorage.removeItem('lightTheme');
+		}
+	});
+
+	delete root.dataset.theme;
+}
